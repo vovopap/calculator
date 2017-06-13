ead41cff1a864480e4784cd8e5a0bd0d7c524581
Vohidjon <abduvohid.karimjonov@gmail.com>
2017-06-14 02:48:55 +0500

    margin calculator implemented stable version

--- src/app/static_margin.ts
+++ src/app/static_margin.ts
@@ -32,11 +32,10 @@ export class Margin {
     }
 
     sync(variable: string) {
-        if(this.lastSyncedVariable !== variable) {
+        if (this.lastSyncedVariable !== variable) {
             this.strongVariable = this.lastSyncedVariable;
         }
         this.lastSyncedVariable = variable;
-        console.log('----------------------------------------------');
         console.log('Priority -> ' + this.strongVariable);
         switch (variable) {
             case 'price': {
@@ -51,61 +50,65 @@ export class Margin {
                 }
 
                 this.margin = this.profit / this.revenue * 100;
-                // this.markup = this.profit / this.price * 100;
+                this.markup = this.profit / this.price * 100;
                 break;
             }
             case 'margin': {
-                /**
-                 * #margin -> markup -> profit -> price
-                 */
                 this.markup = 100 * this.margin / (100 - this.margin);
-
-                this.profit = this.margin * this.revenue / 100;
-
-                this.price = this.revenue - this.profit;
-
+                if (this.price && this.strongVariable === 'price') {
+                    this.profit =  this.price * this.markup / 100;
+                    this.revenue = this.price + this.profit;
+                } else if (this.revenue && this.strongVariable === 'revenue') {
+                    this.price = this.revenue * (100 - this.margin) / 100;
+                    this.profit = this.revenue - this.price;
+                } else if (this.profit && this.strongVariable === 'profit') {
+                    this.revenue = this.profit * 100 / this.margin;
+                    this.price = this.revenue - this.profit;
+                }
                 break;
             }
             case 'markup': {
-                /**
-                 * #markup -> margin -> profit -> price
-                 */
                 this.margin = 100 * this.markup / (100 + this.markup);
-
-                this.profit = this.margin * this.revenue / 100;
-
-                this.price = this.revenue - this.profit;
-
+                if (this.price && this.strongVariable === 'price') {
+                    this.profit =  this.price * this.markup / 100;
+                    this.revenue = this.price + this.profit;
+                } else if (this.revenue && this.strongVariable === 'revenue') {
+                    this.price = this.revenue * (100 - this.margin) / 100;
+                    this.profit = this.revenue - this.price;
+                } else if (this.profit && this.strongVariable === 'profit') {
+                    this.revenue = this.profit * 100 / this.margin;
+                    this.price = this.revenue - this.profit;
+                }
                 break;
             }
             case 'revenue': {
-                if (this.price) {
-                    this.profit = this.revenue - this.price;
-                } else if (this.profit) {
-                    this.price = this.revenue - this.profit;
-                }
+                // if (this.price) {
+                //     this.profit = this.revenue - this.price;
+                // } else if (this.profit) {
+                //     this.price = this.revenue - this.profit;
+                // }
 
                 if (this.price && this.strongVariable === 'price') {
                     this.profit = this.revenue - this.price;
                 } else if (this.profit && this.strongVariable === 'profit') {
                     this.price = this.revenue - this.profit;
-                } else if (this.margin && this.strongVariable === 'margin') {
-                    this.revenue = this.price * 100 / (100 - this.margin);
+                } else if (this.margin && ((this.strongVariable === 'margin') || (this.strongVariable === 'markup'))) {
+                    this.price = this.revenue * (100 - this.margin) / 100;
                     this.profit = this.revenue - this.price;
                     break;
                 }
 
                 this.margin = this.profit / this.revenue * 100;
-                // this.markup = this.profit / this.price * 100;
+                this.markup = this.profit / this.price * 100;
                 break;
             }
             case 'profit': {
                 /**
                  * #profit -> revenue -> margin, markup
                  */
-                if (this.price) {
+                if (this.price && this.strongVariable === 'price') {
                     this.revenue = this.price + this.profit;
-                } else if (this.revenue) {
+                } else if (this.revenue && this.strongVariable === 'revenue') {
                     this.price = this.revenue - this.profit;
                 }
 
